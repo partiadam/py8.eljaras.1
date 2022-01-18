6884ebd0225d3ee87477072a0141671bbc03251b
partiadam <71691758+partiadam@users.noreply.github.com>
2022-01-18 14:15:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+1. Feladat
+Írj eljárást, amely egy tetszőleges szöveget, ill. alakzatot ír ki a képernyőre!
+'''
+
+def szoveg(nev, szoveg, alakzat):
+   print('Szia ' + nev + ' írj valamit: ' + szoveg + ', illetve alakzatot is: '+ alakzat)
+
+szoveg('Ádám', 'szia, örülök hogy találkoztunk', '\n*\n**\n**\n**|**\n**\n**\n*')
